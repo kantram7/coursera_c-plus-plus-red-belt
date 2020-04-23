4c388d087f5435b3521fe7836ff7a17b90f85806
Roman Fedoseev <63730899+kantram7@users.noreply.github.com>
2020-04-23 04:24:15 +0300

    Update print_values.cpp

--- week-1/print_values/print_values.cpp
+++ week-1/print_values/print_values.cpp
@@ -1,1 +1,19 @@
+#include "test_runner.h"
 
+#include <ostream>
+using namespace std;
+
+#define PRINT_VALUES(out, x, y) { out << (x) << endl; out << (y) << endl; }
+// or #define PRINT_VALUES(out, x, y) out << (x) << endl << out << (y) << endl
+
+int main() {
+  TestRunner tr;
+  tr.RunTest([] {
+    ostringstream output;
+    int n = 5;
+    // при использовании неверного макроса ниже будет ошибка
+    if(true) PRINT_VALUES(output, ++n, "red belt")
+    else PRINT_VALUES(output, --n, "red belt");
+    ASSERT_EQUAL(output.str(), "6\nred belt\n");
+  }, "PRINT_VALUES usage example");
+}
