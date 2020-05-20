442432d2fba3bc76899ece1002ae831bd3d7a683
Roman Fedoseev <63730899+kantram7@users.noreply.github.com>
2020-05-20 23:59:01 +0300

    Update print_values.cpp

--- week-1/print_values/print_values.cpp
+++ week-1/print_values/print_values.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 #define PRINT_VALUES(out, x, y) { out << (x) << endl; out << (y) << endl; }
-// or #define PRINT_VALUES(out, x, y) out << (x) << endl << out << (y) << endl
+// or #define PRINT_VALUES(out, x, y) out << (x) << endl << (y) << endl
 
 int main() {
   TestRunner tr;
